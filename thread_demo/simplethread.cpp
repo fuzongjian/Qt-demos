5820ce8bfc9929503a3211335318d52dd1c18a1a
fuzongjian <fu_zongjian@163.com>
2018-01-08 14:53:07 +0800

    测试emit

--- thread_demo/simplethread.cpp
+++ thread_demo/simplethread.cpp
@@ -7,10 +7,10 @@ SimpleThread::SimpleThread()
 void SimpleThread::run(){
     while(count <= 5){
         count ++;
-        qDebug()<<currentThread()<<" id === "<<currentThreadId();
         if(count == 5){
-            emit well_done();
+//            emit well_done();
         }
+        qDebug()<<currentThread()<<" id === "<<currentThreadId();
         sleep(5);
     }
 }
