10af610be96172b2c993667e182aaecc0d873eca
fuzongjian <fu_zongjian@163.com>
2019-01-07 18:23:52 +0800

    qml相关（组件、布局等）

---
+++ QmlStudy/Component-demo/Button.qml
@@ -0,0 +1,13 @@
+﻿import QtQuick 2.0
+
+Rectangle{
+    property alias text: textItem.text
+    width: 100
+    height: 62
+    color: "blue"
+    Text {
+        id: textItem
+        anchors.centerIn: parent
+        color: "white"
+    }
+}
---
+++ QmlStudy/Component-demo/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
